a9d16fe39b81948c743876ef4699c2079127804d
rlawlsdn263 <rlawlsdn263@naver.com>
2023-03-05 12:06:59 +0900

    [Fix] undefined 에러 해결
    - 비동기통신으로 데이터를 가져오기 전에 데이터를 호출해서 발생하는 에러
    - 조건부 렌더링으로 data가 있을 경우 ResultWrap 컴포넌트를 렌더링하게 만듦
    - Object.keys(result).length !==0으로 객체의 값 유무를 확인한 다음 && 연산자로 ResultWrap 컴포넌트를 렌더링함

--- src/App.js
+++ src/App.js
@@ -33,11 +33,13 @@ function App() {
           type="text"
           onKeyDown={searchWeather}
         />
-        <ResultWrap>
-          <div className="city"></div>
-          <div className="temperature"></div>
-          <div className="weather"></div>
-        </ResultWrap>
+        {Object.keys(result).length !== 0 && (
+          <ResultWrap>
+            <div className="city">{result.data.name}</div>
+            <div className="temperature">{result.data.main.temp}</div>
+            <div className="weather">{result.data.weather[0].main}</div>
+          </ResultWrap>
+        )}
       </div>
     </AppWarp>
   );

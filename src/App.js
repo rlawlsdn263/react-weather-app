610e02f4a5b32c46f99d68c80f8ae909d2c9dc41
rlawlsdn263 <rlawlsdn263@naver.com>
2023-03-05 11:23:24 +0900

    [Add] 기본 마크업 완성
    스타일 컴포넌트를 사용해 AppWrap 컴포넌트의 기본 스타일을 잡고 appContentWrap CSS 클래스 스타일을 입힘.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import styled from "styled-components";
+
 function App() {
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=1144f627ca7622ff01372e0335e00e1b`;
 
-  return <div className="App"></div>;
+  return (
+    <AppWarp>
+      <div className="appContentWrap">
+        <input placeholder="도시를 입력하세요"></input>
+      </div>
+    </AppWarp>
+  );
 }
 
 export default App;
+
+// 스타일 컴포넌트
+const AppWarp = styled.div`
+  width: 100vw;
+  height: 100vh;
+  border: 1px red solid;
+
+  .appContentWrap {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    tanform: translate(-50%, -50%);
+    border: 1px blue solid;
+    padding: 20px;
+  }
+`;
